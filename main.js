d648bbad019b1b2930713f66a7431867926caca5
ealop123 <35442165+ealop123@users.noreply.github.com>
2018-03-06 15:04:29 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+var $ = function(id) {
+  return document.getElementById(id);
+}
+
+function replaceClass(_class, _html) {
+  var _classValue = document.getElementsByClassName(_class);
+  [].slice.call(_classValue).forEach(function(_classValue) {
+    _classValue.innerHTML = _html;
+  });
+}
+
+function replaceID(_id, _html) {
+  $(_id).innerHTML = _html;
+}
+
+function testRun() {
+  replaceID("introMessage", 'This is a debugging atmosphere.');
+  replaceClass('userName', 'Fabio');
+  replaceClass('useTime', '3 minutes');
+  replaceClass('pagesPrinted', '10  ');
+  var toDate = new Date();
+  var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour:"numeric", minute:"numeric" };
+  replaceID( 'timestamp', toDate.toLocaleDateString("en-US", options) );
+
+}
+
+function main() {
+  replaceClass('userName', _userName);
+  replaceClass('useTime', _useTime);
+  replaceClass('pagesPrinted', _pagesPrinted);
+  replaceID('timestamp', _timestamp);
+}
+
+var refresh = function() {
+  //update information
+  console.log('refresh works!');
+}
+
+
+window.onload = function() {
+  $('refreshButton').onclick = refresh;
+  main();
+  //testRun();
+}
