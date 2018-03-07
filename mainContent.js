816f0df513294f1a5d5b74d382bdc31a6c023ce3
ealop123 <35442165+ealop123@users.noreply.github.com>
2018-03-06 16:04:55 -0800

    Create mainContent.js

---
+++ mainContent.js
@@ -0,0 +1,7 @@
+/*
+I need _userName, _useTime, _timestamp, and _pagesPrinted to be initalized here
+*/
+var _userName = 'Rachel';
+var _useTime = '2 minutes';
+var _timestamp = 'EXIT TIME';
+var _pagesPrinted = 1;
