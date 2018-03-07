38cc6fed233bdbed113da033cf2b5c08cce7770e
ealop123 <35442165+ealop123@users.noreply.github.com>
2018-03-06 16:23:21 -0800

    Update mainContent.js

--- mainContent.js
+++ mainContent.js
@@ -1,7 +1,7 @@
 /*
 I need _userName, _useTime, _timestamp, and _pagesPrinted to be initalized here
 */
-var _userName = 'Someone';
+var _userName = 'Me';
 var _useTime = '5 minutes';
 var _timestamp = 'EXIT TIME';
 var _pagesPrinted = 10;
